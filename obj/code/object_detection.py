d96b04d992b5ddf842d3fe42d3aa926afc504750
ghanemx <101287079+ghanemx@users.noreply.github.com>
2022-03-14 09:17:56 +0200

    Add files via upload

--- obj/code/object_detection.py
+++ obj/code/object_detection.py
@@ -6,12 +6,15 @@ import cv2
 from main.object_detection import draw_bbox
 from main.object_detection import line_to_speak
 from main.object_detection import detect_common_objects
+import time
+start = time.process_time()
+# your code here
 
 # read input image
-image = cv2.imread(r'.\images\spoon1.jpeg')
+image = cv2.imread(r'.\images\test3.jpeg')
 
 # apply object detection
-bbox, label, conf = detect_common_objects(image,model='yolov4')
+bbox, label, conf = detect_common_objects(image,model='yolov4-tiny')
 
 #line to speak
 text=line_to_speak(label)
@@ -27,8 +30,10 @@ out = draw_bbox(image, bbox, label, conf)
 # press any key to close window           
 cv2.imshow("object_detection", out)
 cv2.waitKey()
+'''
+
+print(time.process_time() - start)
 
 # release resources
 cv2.destroyAllWindows()
 
-'''
